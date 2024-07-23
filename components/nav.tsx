6d9c2ff192ed5a6dcc7cbfb55c23b7a1b183099c
incipitluis <incipitluis@gmail.com>
2024-07-23 14:16:14 +0200

    fix: resolve menu disappearance and scroll issues on small screens
    - Ensure menu toggle closes properly on screen resize - Correctly manage body overflow to enable/disable scroll when menu is open/closed

--- components/nav.tsx
+++ components/nav.tsx
@@ -1,31 +1,26 @@
-'use client'
+"use client";
 
-import React, { useEffect } from 'react';
-import Link from 'next/link';
-import { ModeToggle } from './ui/mode-toggle';
-import Image from 'next/image';
+import React, { useEffect } from "react";
+import Link from "next/link";
+import { ModeToggle } from "./ui/mode-toggle";
+import Image from "next/image";
 import { useToggle, useWindowSize } from "react-use";
-import { Button } from './ui/button';
-import { MenuIcon, XIcon } from 'lucide-react';
-import { MobileModeToggle } from './ui/mobile-mode-toggle';
-import {
-  SignInButton,
-  SignedIn,
-  SignedOut,
-  UserButton
-} from '@clerk/nextjs'
+import { Button } from "./ui/button";
+import { MenuIcon, XIcon } from "lucide-react";
+import { MobileModeToggle } from "./ui/mobile-mode-toggle";
+import { SignInButton, SignedIn, SignedOut, UserButton } from "@clerk/nextjs";
 
 const Nav = () => {
   const [isMenuOpen, toggleMenu] = useToggle(false);
-  const {width} = useWindowSize()
+  const { width } = useWindowSize();
 
-  useEffect(
-    ()=>{
-      if (isMenuOpen && width>768){
-        toggleMenu(false)
-      }
-    },[width]
-  );
+  useEffect(() => {
+    if (width > 768 && isMenuOpen) {
+      toggleMenu(false);
+      document.body.style.overflow = "";
+    }
+  }, [width, isMenuOpen, toggleMenu]);
+
   useEffect(() => {
     if (isMenuOpen) {
       document.body.style.overflow = "hidden";
@@ -37,7 +32,13 @@ const Nav = () => {
   return (
     <nav className="w-full bg-stone-700 bg-opacity-50 dark:bg-opacity-80 shadow-xl dark:bg-gray-800 text-white py-4">
       <div className="mx-auto flex justify-between items-center px-6 md:px-16">
-        <Image src={"/ink-spot.png"} alt='logo of Ink Spot Tattoo' width={92} height={92}></Image>
+        <Image
+          src={"/logo.svg"}
+          alt="logo of Ink Spot Tattoo"
+          width={92}
+          height={92}
+          className="dark:bg-white bg-yellow-600"
+        ></Image>
         <ModeToggle></ModeToggle>
         <MobileModeToggle></MobileModeToggle>
         <div className="hidden md:flex flex-row text-xl font-bold space-y-0 space-x-8 mt-0">
@@ -61,29 +62,49 @@ const Nav = () => {
           <UserButton />
         </SignedIn>
         <Button
-            variant={"secondary"}
-            className="rounded-full h-[36px] w-[36px] px-2 md:hidden"
-            onClick={toggleMenu}
-          >
-            {isMenuOpen ? <XIcon /> : <MenuIcon />}
-          </Button> 
+          variant={"secondary"}
+          className="rounded-full h-[36px] w-[36px] px-2 md:hidden"
+          onClick={toggleMenu}
+        >
+          {isMenuOpen ? <XIcon /> : <MenuIcon />}
+        </Button>
       </div>
       {isMenuOpen && (
         <div className="top-16 mt-[-2px] left-0 h-screen w-screen shadow-lg px-6 pb-16">
           <div className="flex flex-col max-w-sm mx-auto h-full">
-            <ul className="h-full text-right text-xl font-bold flex flex-col justify-evenly">  
-              <li><Link href="/" className="hover:underline">
-            Home
-          </Link></li>
-              <li><Link href="/gallery" className="hover:underline">
-            Gallery
-          </Link></li> 
-              <li><Link href="/about" className="hover:underline">
-            Our philosophy
-          </Link></li> 
-              <li><Link href="/contact" className="hover:underline">
-            Reach out!
-          </Link></li>  
+            <ul className="h-full text-right text-xl font-bold flex flex-col justify-evenly">
+              <li>
+                <Link href="/" className="hover:underline" onClick={toggleMenu}>
+                  Home
+                </Link>
+              </li>
+              <li>
+                <Link
+                  href="/gallery"
+                  className="hover:underline"
+                  onClick={toggleMenu}
+                >
+                  Gallery
+                </Link>
+              </li>
+              <li>
+                <Link
+                  href="/about"
+                  className="hover:underline"
+                  onClick={toggleMenu}
+                >
+                  Our philosophy
+                </Link>
+              </li>
+              <li>
+                <Link
+                  href="/contact"
+                  className="hover:underline"
+                  onClick={toggleMenu}
+                >
+                  Reach out!
+                </Link>
+              </li>
             </ul>
           </div>
         </div>
